7482f71f83fef8b0299e8eb2b04f5b0254fef36f
orion GONZALEZ (contractor) <orion.gonzalez@amadeus.com>
2024-03-08 02:14:51 +0100

    full rework

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-// #[derive(Parser)]
-// struct Cli {}
-
 use std::{
-    fs::{File, OpenOptions},
+    fs::{read_to_string, File, OpenOptions},
     path::Path,
-    process::Output,
     sync::Arc,
     time::Duration,
 };
@@ -12,127 +8,81 @@ use std::{
 use anyhow::{anyhow, Context};
 
 use futures::future::join_all;
+use serde::Deserialize;
 use tokio::{process::Command, time::sleep};
-use tracing::{error, info, instrument, trace, Level};
 use tracing_subscriber::layer::SubscriberExt;
 
-struct Config {}
+#[derive(Debug, Deserialize)]
+struct Task {
+    command: String,
+    duration: String,
+}
 
-#[instrument]
-async fn loop_folder(base: &Path, folder_name: &str, duration: Duration) -> () {
-    let name = base.join(folder_name);
-    loop {
-        let span = tracing::span!(Level::INFO, "loop_folder", frequency = folder_name);
-        let guard = span.enter();
-        trace!("reading {folder_name}");
-        let files = walkdir::WalkDir::new(&name).into_iter();
+#[derive(Debug, Deserialize)]
+struct Tasks {
+    entries: Vec<Task>,
+}
 
-        let mut futs = vec![];
-        for file in files {
-            let file = match file {
-                Ok(e) => e,
-                Err(e) => {
-                    error!("error reading file: {e}");
-                    continue;
-                }
-            };
-            if !file.file_type().is_file() {
-                continue;
+fn parse_time(input: &str) -> Duration {
+    let mut input = input.split(':');
+    let hours: u64 = input.next().unwrap().parse().unwrap();
+    let mins: u64 = input.next().unwrap().parse().unwrap();
+    let secs: u64 = input.next().unwrap().parse().unwrap();
+    assert!(input.next().is_none(), "bad format");
+    Duration::from_secs(hours * 3600 + mins * 60 + secs)
+}
+
+async fn task_loop(Tasks { entries }: Tasks) {
+    let mut handles = vec![];
+    for Task { command, duration } in entries {
+        let duration = parse_time(&duration);
+        let t = tokio::spawn(async move {
+            loop {
+                _ = Command::new("pwsh.exe")
+                    .arg("-Command")
+                    .arg(&command)
+                    .spawn()
+                    .unwrap()
+                    .wait()
+                    .await;
+                sleep(duration).await;
             }
-            let file = file.path().to_owned();
-
-            let cmd;
-            cfg_if::cfg_if! {
-                if #[cfg(windows)] {
-                    cmd = Command::new("pwsh.exe")
-                        .arg("-File")
-                        .arg(file.as_os_str())
-                        .spawn();
-                    trace!("{cmd:?}");
-                } else {
-                    todo!("untested on other platforms, should respect shebang");
-                     cmd = Command::new(&file).spawn();
-                }
-            };
-
-            let child = match cmd {
-                Ok(e) => e,
-                Err(e) => {
-                    error!("error executing {:?}: {e}", file);
-                    continue;
-                }
-            };
-
-            futs.push(async move { (child.wait_with_output().await, file) });
-        }
-        drop(guard);
-
-        let futs = join_all(futs).await;
-        for (res, path) in futs {
-            let path = path.as_os_str().to_str().unwrap_or("");
-            let span = tracing::span!(Level::INFO, "loop_folder", file_name = path);
-            _ = span.enter();
-
-            let Output {
-                status,
-                stdout,
-                stderr,
-            } = match res {
-                Ok(e) => e,
-                Err(e) => {
-                    error!("error executing {path:?}: {e}");
-                    continue;
-                }
-            };
-            let stdout = String::from_utf8_lossy(&stdout);
-            let stderr = String::from_utf8_lossy(&stderr);
-            if status.code().unwrap_or(1) == 0 {
-                info!("{stdout}");
-                info!("{stderr}");
-            } else {
-                error!("{stdout}");
-                error!("{stderr}");
-            }
-        }
-
-        sleep(duration).await;
+        });
+        handles.push(t);
     }
+    join_all(handles.into_iter()).await;
 }
 
 async fn cron_loop(base: &Path) {
-    let proms = vec![
-        loop_folder(base, "second", Duration::from_secs(1)),
-        loop_folder(base, "minute", Duration::from_secs(60)),
-        loop_folder(base, "hour", Duration::from_secs(60 * 60)),
-        loop_folder(base, "day", Duration::from_secs(60 * 60 * 24)),
-        loop_folder(base, "week", Duration::from_secs(60 * 60 * 24 * 7)),
-    ];
-    join_all(proms).await;
+    let file = read_to_string(base).unwrap();
+    let tasks: Tasks = toml::from_str(&file).unwrap();
+
+    task_loop(tasks).await;
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let home = homedir::get_my_home()?.context("No homedir")?;
-    let mut base = home.join(".crond");
+    let mut base = home.join(".crond.toml");
     if !base.exists() {
-        base = home.join(".config").join("crond");
+        base = home.join(".config").join("crond.toml");
     }
 
     if !base.exists() {
         Err(anyhow!(
-            "Error: Expected folder {:?} or {:?}",
+            "Error: Expected file {:?} or {:?}",
             home.join(".crond"),
             base
         ))?
     }
-    if base.is_file() {
-        Err(anyhow!("Error: {base:?} is a file",))?
+    if base.is_dir() {
+        Err(anyhow!("Error: {base:?} is a dir",))?
     }
 
     let stdout_log = tracing_subscriber::fmt::layer().pretty();
     let subscriber = tracing_subscriber::Registry::default().with(stdout_log);
 
-    let logfile = base.join("logfile.txt");
+    let logfile = base.parent().unwrap().join("crond.log");
     let logfile = if logfile.exists() {
         OpenOptions::new().append(true).open(logfile)
     } else {
@@ -144,6 +94,7 @@ async fn main() -> anyhow::Result<()> {
         .with_writer(Arc::new(logfile));
     let subscriber = subscriber.with(layer);
     tracing::subscriber::set_global_default(subscriber)?;
+
     cron_loop(&base).await;
     Ok(())
 }
